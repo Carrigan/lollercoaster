0210764bb5538aca912db53a9e30262aab6759a4
Carrigan <brian.c.carrigan@gmail.com>
2015-01-09 22:07:31 -0500

    Added earth generation.

---
+++ lollercoaster/ground.py
@@ -0,0 +1,7 @@
+import random
+
+grass_tiles = [",", ";", "\"", "`", " "]
+
+def generate_earth(depth, length):
+    for row in xrange(depth):
+        print "".join([random.choice(grass_tiles) for x in range(length)])
--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 from lollercoaster.generator import generate_block
+from lollercoaster.ground import generate_earth
 
 generate_block(16, 20)
+generate_earth(2, 16)
+
