11ac15940888ed1a6662e2d0794fc5972712806e
Carrigan <brian.c.carrigan@gmail.com>
2015-01-11 17:34:28 -0500

    Created a more realistic state based level generator.

--- lollercoaster/generator.py
+++ lollercoaster/generator.py
@@ -7,13 +7,15 @@ class ColumnBuilder(object):
         self.x = 0
         self.max_height = max_height
         self.last_column = None
+        self.direction_count = 0
 
     def create_initial_column(self):
         self.x = 0
-        initial_height = random.randint(0, self.max_height)
+        initial_height = random.randint(0, self.max_height - 1)
         current_type = random.randint(-1, 1)
         self.last_column = Column(initial_height, current_type, self.max_height)
         self.last_column.fill(pattern_types[self.x % 3])
+        self.direction_count += 1
         return self.last_column
 
     def create_next_column(self):
@@ -21,18 +23,35 @@ class ColumnBuilder(object):
         height = self.last_column.height
         last_type = self.last_column.column_type
 
-        current_type = random.randint(-1, 1)
-
+        # If the last one was going up, this one's height will be increased
         if last_type == 1:
             height += 1
 
+        # If we have been going up or down long enough, give it 50/50 to continue or flatten out
+        current_type = None
+        if self.direction_count < 2:
+            current_type = last_type
+        else:
+            if last_type != 0:
+                current_type = last_type if random.choice([True, False]) else 0
+            else:
+                current_type = current_type = random.randint(-1, 1)
+
+        # Range check lower
         if current_type == -1:
             if height == 0:
                 current_type = 0
             height += current_type
 
+        # Range check upper
         if current_type == 1 and height == self.max_height - 2:
             current_type = 0
+
+        # After all that, if the direction changed, reset the count
+        if current_type != last_type:
+            self.direction_count = 0
+        else:
+            self.direction_count += 1
 
         self.last_column = Column(height, current_type, self.max_height)
         self.last_column.fill(pattern_types[self.x % 3])
